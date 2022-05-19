348af9ba95aefec8c9731973bdfbd3e5b0695253
camila <tec.camiladossantos@gmail.com>
2022-05-19 17:28:05 -0300

    pasta

--- myCodes/Aula03/main/1-Exercicio.c
+++ myCodes/Aula03/main/1-Exercicio.c
@@ -13,27 +13,29 @@ solto em 2 Hz*/
 
 #define PIN_BUTTON 15
 #define PIN_LED 23 /* mantem o botão precionado "gpio_get_level(PIN_BUTTON_)"*/
+xSemaphoreHandle mutexBus; 
 int counter = 0;
-int mutexBus = 0;
 int level = 0;
 const char* TAG ="1-Exercicio";
 
 void task1(void *param)
 {
+  
     while (true)
     {
+       
         int level = gpio_get_level(PIN_BUTTON);
         if(level == 1)
             {
-                xSemaphore(mutexBus, 0);
+                xSemaphoreTake(mutexBus, 0);
                 counter = 1;
-                xSemaphore(mutexBus);       
+                xSemaphoreGive(mutexBus);       
             }
         else
         {
-            xSemaphore(mutexBus, 0);
+            xSemaphoreTake(mutexBus, 0);
             counter = 0;
-            xSemaphore(mutexBus);       
+            xSemaphoreGive(mutexBus);       
         }
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
@@ -41,23 +43,23 @@ void task1(void *param)
 }
 void task2(void *param)
 {
+   bool isOn = 0;
     while (true)
     {
-        
+       isOn = !isOn;
         if(counter ==1)
             {
+
                 
-            gpio_set_level(PIN_LED, 1);     // ligar led
-            vTaskDelay(pdMS_TO_TICKS(250));
-            gpio_set_level(PIN_LED, 0);
-            vTaskDelay(pdMS_TO_TICKS(250));
+            gpio_set_level(PIN_LED, isOn);     // ligar led
+            vTaskDelay(pdMS_TO_TICKS(50));
+        
             }
         else
         {
-            gpio_set_level(PIN_LED, 1); // liga o led
-            vTaskDelay(pdMS_TO_TICKS(50));
-            gpio_set_level(PIN_LED, 0);
-            vTaskDelay(pdMS_TO_TICKS(50));
+            gpio_set_level(PIN_LED, isOn); // liga o led
+            vTaskDelay(pdMS_TO_TICKS(250));
+           
         }
     }
     vTaskDelete(NULL);
@@ -78,11 +80,10 @@ void app_main()
     gpio_pulldown_en(PIN_BUTTON);
     gpio_pullup_dis(PIN_BUTTON);
 
- 
-    
      mutexBus = xSemaphoreCreateMutex();
-     xTaskCreatePinnedToCore(&task1, "temperature reading", 2048, NULL, 2, NULL,0);
-    xTaskCreatePinnedToCore(&task2, "humidity reading", 2048, NULL, 2, NULL, 1);
+
+     xTaskCreate(&task1, "temperature reading", 2048, NULL, 2, NULL);
+     xTaskCreate(&task2, "humidity reading", 2048, NULL, 2, NULL);
     }
 
 
